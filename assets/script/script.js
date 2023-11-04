a26b305c982521cacbdf0449f5f69b0ee9820d8d
Festin bryan <festinbryan7@gmail.com>
2023-11-04 15:19:55 +0100

    projet terminé (normalement)

--- assets/script/script.js
+++ assets/script/script.js
@@ -6,11 +6,11 @@ search.addEventListener('keyup', function(event) {
       fetchData();
       document.querySelector('.moveDiv').style.bottom="40vh";
       document.querySelector('.moveMain').style.bottom="60vh";
-
+      document.querySelector('.moveMain').style.visibility="visible";
     }
   });
 
-  fetch(`/weather.json`);
+  fetch(`weather.json`);
 
   function  fetchData(){
     const searchBox = document.querySelector('.main-container');
@@ -22,7 +22,7 @@ search.addEventListener('keyup', function(event) {
 
 let cityValue = document.querySelector('.input-search').value;
 
-fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityValue}&appid=e0e81ee2e76187ade4f461b4e4485d54`,)
+fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityValue}&units=metric&appid=e0e81ee2e76187ade4f461b4e4485d54`,)
 
 .then (response => response.json())
 .then (data => {
@@ -33,11 +33,27 @@ fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityValue}&appid=e0e
     console.log(city.dt.txt);
     container.innerHTML = `
     <h2>${data.city.name}</h2>
-    <p>${data.list[2].dt_txt}, ${data.list[6].weather[0].icon}</p>
-    <p>${data.list[3].dt_txt}, ${data.list[6].weather[0].icon}</p>
-    <p>${data.list[4].dt_txt}, ${data.list[6].weather[0].icon}</p>
-    <p>${data.list[5].dt_txt}, ${data.list[6].weather[0].icon}</p>
-    <p>${data.list[6].dt_txt}, ${data.list[6].weather[0].icon}</p>
+    <div class="weatherDiv">
+    <p><span>Date : </span>${data.list[7].dt_txt}</p>
+    <p><span>Temperature : </span>${data.list[7].main.temp} °C</p>
+    </div>
+    <div class="weatherDiv">
+    <p><span>Date : </span>${data.list[8].dt_txt}</p>
+    <p><span>Temperature : </span>${data.list[8].main.temp} °C</p>
+    </div>
+    <div class="weatherDiv">
+    <p><span>Date : </span>${data.list[9].dt_txt}</p>
+    <p><span>Temperature : </span>${data.list[9].main.temp} °C</p>
+    </div>
+    <div class="weatherDiv">
+    <p><span>Date : </span>${data.list[10].dt_txt}</p>
+    <p><span>Temperature : </span>${data.list[10].main.temp} °C</p>
+    </div>
+    <div class="weatherDiv">
+    <p><span>Date : </span>${data.list[11].dt_txt}</p>
+    <p><span>Temperature : </span>${data.list[11].main.temp} °C</p>
+     
+    </div>
 
   
 
